5ade1d8a0d15ad2d593e073a2411f91d7eb1cc06
Levente Mészáros <levy@omnetpp.org>
2016-07-11 11:16:21 +0200

    Link: Determine network protocol from ProtocolInd tag.

--- src/inet/linklayer/ppp/PPP.cc
+++ src/inet/linklayer/ppp/PPP.cc
@@ -414,9 +414,8 @@ void PPP::updateDisplayString()
 
 PPPFrame *PPP::encapsulate(cPacket *msg)
 {
-    IMACProtocolControlInfo *controlInfo = check_and_cast<IMACProtocolControlInfo *>(msg->getControlInfo());
     PPPFrame *pppFrame = new PPPFrame(msg->getName());
-    pppFrame->setProtocol(controlInfo->getNetworkProtocol());
+    pppFrame->setProtocol(ProtocolGroup::ethertype.getProtocolNumber(msg->getMandatoryTag<ProtocolInd>()->getProtocol()));
     pppFrame->setByteLength(PPP_OVERHEAD_BYTES);
     pppFrame->encapsulate(msg);
     return pppFrame;
